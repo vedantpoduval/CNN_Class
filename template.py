cdbddff2da8055412426021b28ba681a6b8662be
Hrishikesh Poduval <hrishikeshpoduval08@Hrishikeshs-MacBook-Air.local>
2024-03-13 19:23:46 +0530

    project template added

---
+++ template.py
@@ -0,0 +1,37 @@
+import os
+from pathlib import Path
+import logging
+
+#This is the format of logging which we expect
+logging.basicConfig(level = logging.INFO,format = '[%(asctime)s]: %(message)s:')
+
+proj_name = "cnn_classifier"
+files = [
+    ".github/workflows/.gitkeep", #This is for implementing the github actions
+    f"src/{proj_name}/__init__.py",
+    f"src/{proj_name}/utils/__init__.py",
+    f"src/{proj_name}/components/__init__.py",
+    f"src/{proj_name}/config/__init__.py",
+    f"src/{proj_name}/pipeline/__init__.py",
+    f"src/{proj_name}/entity/__init__.py",
+    f"src/{proj_name}/constants/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb"
+]
+
+for file in files:
+    dir,dir_file = os.path.split(file)
+
+    if dir != "":
+        os.makedirs(dir,exist_ok=True)
+        logging.info(f"Creating the directory: {dir} for file: {dir_file}")
+    if (not os.path.exists(file)) or (os.path.getsize(file) == 0):
+        with open(file,'w') as f:
+            pass
+            logging.info(f"Creating empty file {dir_file}")
+    else:
+        logging.info(f"{dir_file} already exists")
+
